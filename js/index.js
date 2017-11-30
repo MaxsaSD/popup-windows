74fb2fb7175a983061a038f76101830696c965fc
MaxsaSD <maxsa.lviv@gmail.com>
2017-11-30 20:52:58 +0000

    First Edition

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(function (){
+    if ($.cookie('swnd')) {
+        let delay_popup = 5000;
+        setTimeout("document.getElementById('mx-wnd-container').style.display='block'", delay_popup);
+    }
+    $.cookie('swnd', true, {expires: 7, path: '/'});
+});
+
+function mxWndClose() {
+    document.getElementById("mx-wnd-wnd").className="bounceOutDown animated";
+    setTimeout("document.getElementById('mx-wnd-container').style.display='none'", 1000);
+    //document.getElementById('mx-wnd-container').style.display='none';
+}
+
